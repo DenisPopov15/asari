e32dd6f2e14db8573eb620040606370788130ed1
nkumeiko <nkumeiko@gmail.com>
2015-05-07 23:37:56 +0800

    use the new api syntax

--- lib/asari.rb
+++ lib/asari.rb
@@ -67,12 +67,18 @@ class Asari
     return Asari::Collection.sandbox_fake if self.class.mode == :sandbox
     term,options = "",term if term.is_a?(Hash) and options.empty?
 
-    bq = boolean_query(options[:filter]) if options[:filter]
+    if options[:filter]
+      bq = boolean_query(options[:filter])
+      bq = "(and '#{term.to_s.gsub("'", " ")}' #{bq})" if term.present?
+    end
     page_size = options[:page_size].nil? ? 10 : options[:page_size].to_i
 
     url = "http://search-#{search_domain}.#{aws_region}.cloudsearch.amazonaws.com/#{api_version}/search"
-    url += "?q=#{CGI.escape(term.to_s)}"
-    url += "&bq=#{CGI.escape(bq)}" if options[:filter]
+    if options[:filter]
+      url += "?q.parser=structured&q=#{CGI.escape(bq)}"
+    else
+      url += "?q=#{CGI.escape(term.to_s)}"
+    end
     url += "&size=#{page_size}"
     url += "&return-fields=#{options[:return_fields].join ','}" if options[:return_fields]
 
